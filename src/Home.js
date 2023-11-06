adf0981585b9ed5efe4c1c9406542aa8ca090fe5
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-11-06 10:45:00 +0100

    lagt til css fra OG project

--- src/Home.js
+++ src/Home.js
@@ -3,11 +3,13 @@ const Home = ({ changeView, currentView }) => {
 
     return (
         <div className="home">
-            <button className="topButton" onclick="filterView()" style="height: 75px; width: 100px; font-size: large;">Filter</button>
+            <button className="topButton">Filter</button>
             <h1>Choose a random FFXIV job!</h1>
             <div>
-                <button class="jobButton" onClick={() => changeView('result')}>Find a job</button>
+                <button className="jobButton" onClick={() => changeView('result')}>Find a job</button>
             </div>
         </div>
     )
 }
+
+export default Home;
