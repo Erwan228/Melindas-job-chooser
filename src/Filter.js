00f5f1fa1d0ee3f3988d80ab4ec421af6729b464
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-11-14 14:48:01 +0100

    tirsdag

--- src/Filter.js
+++ src/Filter.js
@@ -39,19 +39,67 @@ const Filter = ({ changeView, currentView, jobs }) => {
             <button className="tankButton biggerButton" onClick={tankFilter}>Tank</button><br />
             <button className="healerButton biggerButton" onClick={healerFilter}>Healer</button><br />
             <button className="regenButton healerButton biggerButton" onClick={regenFilter}>Regen</button>
-            <button className="barrierButton healerButton biggerButton" onClick={barrierFilter}>Tank</button><br />
-            <button className="DPSButton biggerButton" onClick={DPSFilter}>Tank</button><br />
-            <button className="meleeButton DPSButton biggerButton" onClick={meleeFilter}>Tank</button>
-            <button className="rangeButton DPSButton biggerButton" onClick={rangeFilter}>Tank</button>
-            <button className="magicButton DPSButton biggerButton" onClick={magicFilter}>Tank</button>
-            <div className="checks">
+            <button className="barrierButton healerButton biggerButton" onClick={barrierFilter}>Barrier</button><br />
+            <button className="DPSButton biggerButton" onClick={DPSFilter}>DPS</button><br />
+            <button className="meleeButton DPSButton biggerButton" onClick={meleeFilter}>Melee</button>
+            <button className="rangeButton DPSButton biggerButton" onClick={rangeFilter}>Range</button>
+            <button className="magicButton DPSButton biggerButton" onClick={magicFilter}>Magic</button>
+            <hr />
+            <button className="biggerButton" onClick="toggleFilter()">Toggle all on/off</button>
+            <hr />
+            <h2>Check off the jobs individually</h2>
+            <ul className="checks">
                 <div className="tanks">
                     {tankJobs.map((job, index) => (
-                        <li key={index}>   <div><input type="checkBox" ></input>{job.jobName} </div></li>
+                        <span key={index}><div>
+                            <input type="checkBox" ></input>{job.jobName}
+                        </div></span>
                     ))}
                 </div>
-
-            </div>
+                <br />
+                <div className="DPS">
+                    <div className="melee">
+                        {meleeJobs.map((job, index) => (
+                            <span key={index}><div>
+                                <input type="checkBox" ></input>{job.jobName}
+                            </div></span>
+                        ))}
+                    </div>
+                    <br />
+                    <div className="range">
+                        {rangeJobs.map((job, index) => (
+                            <span key={index}><div>
+                                <input type="checkBox" ></input>{job.jobName}
+                            </div></span>
+                        ))}
+                    </div>
+                    <br />
+                    <div className="magic">
+                        {magicJobs.map((job, index) => (
+                            <span key={index}><div>
+                                <input type="checkBox" ></input>{job.jobName}
+                            </div></span>
+                        ))}
+                    </div>
+                </div>
+                <div className="healer">
+                    <div className="regen">
+                        {healerJobs.map((job, index) => (
+                            <span key={index}><div>
+                                <input type="checkBox"></input>{job.jobName}
+                            </div></span>
+                        ))}
+                    </div>
+                    <br />
+                    <div className="barrier">
+                        {barrierJobs.map((job, index) => (
+                            <span key={index}><div>
+                                <input type="checkBox"></input>{job.jobName}
+                            </div></span>
+                        ))}
+                    </div>
+                </div>
+            </ul>
         </div >
     )
 }
