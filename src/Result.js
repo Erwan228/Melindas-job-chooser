c4cb9ca366ca1361a2a1a87cc1432ae0184c5872
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2024-01-04 14:24:35 +0100

    Update Result.js

--- src/Result.js
+++ src/Result.js
@@ -30,14 +30,17 @@ const Result = ({ changeView, msg, filteredJobs }) => {
         newJob()
     }, []);
     return (
-        <div className="Result">
+        <div className="Result" style={{
+            background: Jobs[jobNumber].color,
+            height: 100
+        }}>
             <button className="upperResButton" onClick={() => changeView('filter')}>Filter</button> <p className="rndMsgP">{displayMsg}</p>
             <h1>{Jobs && Jobs[jobNumber] && Jobs[jobNumber].jobName}</h1>
-            <br /><p className="resultInfoP"></p>
+            <br /><p className="resultInfoP">{Jobs && Jobs[jobNumber] && Jobs[jobNumber].info}</p>
             <div>
                 <button className="jobButton" onClick={newJob}>Another job</button>
             </div>
-        </div>
+        </div >
     )
 }
 
